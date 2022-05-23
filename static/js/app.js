57d68c8c01cb31ff675eb231c73ebd1fb8ecf1d3
5t4david <81177362+5t4david@users.noreply.github.com>
2022-05-23 11:24:09 -0500

    filter added

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,39 @@
 // from data.js
 var tableData = data;
 
+  
+// D3 selector
+var tbody = d3.select("tbody");
+var submit = d3.select("#filterButton");
+
+// Update table with a new dataset
+function updateTable(dataset) {
+  tbody.html('');
+  dataset.forEach((toBeDefined) => {
+    var row = tbody.append("tr");
+    Object.entries(toBeDefined).forEach(([key,value]) => {
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
+  });
+}
+
+// Filter date function (just compare a string)
+function filterByDate(dataset) {
+    var filteredData = dataset.filter(function (d) {
+      return d.datetime === $('#datetime').val();
+    });
+    return filteredData;
+}
+
+// Start here ...
+// First update table of original data
+updateTable(data); 
+submit.on("click", function() {
+  // When filter is click
+  // Filter data by datetime and update the table
+  var result = filterByDate(data);
+  updateTable(result);
+});
+
 // YOUR CODE HERE!
